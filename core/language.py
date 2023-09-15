735c85729469a3b9606b464d7c8138ff27ebff1b
Emil4154515 <emil4154515@gmail.com>
2023-09-15 19:21:44 +0400

    lang lower

--- core/language.py
+++ core/language.py
@@ -5,7 +5,7 @@ from starlette_context import context
 
 async def get_language(lang_code: Optional[str] = Header(None)):
     if lang_code is not None and lang_code != '':
-        context['lang'] = lang_code
+        context['lang'] = lang_code.lower()
         return lang_code
     else:
         context['lang'] = 'en'
