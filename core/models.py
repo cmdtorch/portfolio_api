298b5090335c1c96a18cbce083a0792fd829411f
Emil4154515 <emil4154515@gmail.com>
2023-10-03 15:57:36 +0400

    change label

--- core/models.py
+++ core/models.py
@@ -7,8 +7,8 @@ from starlette_context import context
 class BaseClass(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True, blank=True)
-    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(verbose_name='Updated at', auto_now=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='Created at', auto_now_add=True, blank=True)
 
     class Meta:
         abstract = True
